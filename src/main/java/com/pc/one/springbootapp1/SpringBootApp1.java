a45041d4f5773c7d69051861e276ab19ef02cd3f
Paramanagouda Patil <paramanagowda.patil@gmail.com>
2021-09-18 22:19:16 +0530

    updated gitignore file

--- src/main/java/com/pc/one/springbootapp1/SpringBootApp1.java
+++ src/main/java/com/pc/one/springbootapp1/SpringBootApp1.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootApp1Application {
+public class SpringBootApp1 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootApp1Application.class, args);
+        SpringApplication.run(SpringBootApp1.class, args);
     }
 
 }
