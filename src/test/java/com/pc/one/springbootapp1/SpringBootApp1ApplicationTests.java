8e649f4ad48055903683e5a718e9316dc44a3ccd
paramanagouda <36893352+paramanagouda@users.noreply.github.com>
2021-09-18 21:01:30 +0530

    Add files via upload
    Initial Commit

---
+++ src/test/java/com/pc/one/springbootapp1/SpringBootApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pc.one.springbootapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
